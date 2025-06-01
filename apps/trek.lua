801729c3ebfdc3807795bf5c665e3a6709bc8891
Rlimoges <35402321+rlimoges11@users.noreply.github.com>
2025-06-01 12:19:07 -0400

    monitor scaling

--- apps/trek.lua
+++ apps/trek.lua
@@ -3,9 +3,11 @@ local monitor1, monitor2 = peripheral.find("monitor")
 
 local function initMonitors()
     if monitor1 ~= nil then
+        monitor1.setTextScale(0.5)
         mcars(monitor1).start()
     end
     if monitor2 ~= nil then
+        monitor2.setTextScale(0.5)
         mcars(monitor2).start()
     end
     mcars(term).start()
