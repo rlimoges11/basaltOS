fde862bf241e77d059c2a6c22351ea225fbc5d17
Rlimoges <35402321+rlimoges11@users.noreply.github.com>
2025-06-01 16:34:57 -0400

    monitor sizes

--- apps/mcars.lua
+++ apps/mcars.lua
@@ -14,11 +14,6 @@ return function(monitor)
         :setSize(tw, th)
         :setPosition(1,1)
 
-    local frame = main:addFrame()
-        :setSize(tw, th)
-        :setPosition(1,1)
-
-
     function api.drawPanel(x, y, w, h, fg, bg, frame)
         local thisPanel = main:addFrame("panel")
             :setPosition(x, y)
@@ -52,6 +47,11 @@ return function(monitor)
     end
 
     function api.start()
+        -- term: 51
+        -- small : 121
+        -- large : 164
+
+
         local mw, mh = monitor:getSize()
         panels[#panels] = api.drawPanel(1, 1, 40, 20, colors.blue, colors.lightBlue, 4)
 
@@ -62,7 +62,7 @@ return function(monitor)
             :execute("programs/fLogger")
 
         panels[#panels] = api.drawPanel(41, 1, 40, 20, colors.blue, colors.lightBlue, 2)
-        local floggerApp3 = panels[2]:addProgram()
+        local floggerApp2 = panels[2]:addProgram()
             :setSize(38, 16)
             :setPosition(3,5)
             :execute("programs/fLogger")
@@ -73,7 +73,7 @@ return function(monitor)
         panels[#panels] = api.drawPanel(121, 1, 40, 20, colors.blue, colors.lightBlue, 6)
 
         panels[#panels] = api.drawPanel(1, 20, 40, 20, colors.blue, colors.lightBlue, 3)
-        local floggerApp2 = panels[5]:addProgram()
+        local floggerApp3 = panels[5]:addProgram()
             :setSize(33, 16)
             :setPosition(3,3)
             :execute("programs/fLogger")
@@ -82,6 +82,17 @@ return function(monitor)
         panels[#panels] = api.drawPanel(41, 20, 40, 20, colors.blue, colors.lightBlue, 1)
         panels[#panels] = api.drawPanel(81, 20, 40, 20, colors.blue, colors.lightBlue, 5)
         panels[#panels] = api.drawPanel(121, 20, 40, 20, colors.blue, colors.lightBlue, 6)
+
+
+    deb = panels[0]:addFrame()
+        :setSize(12,1)
+        :setPosition(1,1)
+        :setBackground(colors.black)
+    deb:addLabel()
+        :setForeground(colors.white)
+        :setText("w: " .. tw .. " h: " .. th)
+
+
     end
 
     return api, basalt
