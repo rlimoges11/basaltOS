98d0bb0bfe82f0f293a488c9eacfc477bb120307
Rlimoges <35402321+rlimoges11@users.noreply.github.com>
2025-06-01 17:26:45 -0400

    layout progress

--- apps/mcars.lua
+++ apps/mcars.lua
@@ -47,52 +47,76 @@ return function(monitor)
     end
 
     function api.start()
-        -- term: 51
-        -- small : 121
-        -- large : 164
-
-
-        local mw, mh = monitor:getSize()
-        panels[#panels] = api.drawPanel(1, 1, 40, 20, colors.blue, colors.lightBlue, 4)
-
-
-        local floggerApp = panels[1]:addProgram()
-            :setSize(33, 16)
-            :setPosition(3,3)
-            :execute("programs/fLogger")
-
-        panels[#panels] = api.drawPanel(41, 1, 40, 20, colors.blue, colors.lightBlue, 2)
-        local floggerApp2 = panels[2]:addProgram()
-            :setSize(38, 16)
-            :setPosition(3,5)
-            :execute("programs/fLogger")
+        -- pocket : 26, 
+        -- term: 51, 19
+        -- small : 121, 38
+        -- large : 164, 38
 
 
 
-        panels[#panels] = api.drawPanel(81, 1, 40, 20, colors.blue, colors.lightBlue, 5)
-        panels[#panels] = api.drawPanel(121, 1, 40, 20, colors.blue, colors.lightBlue, 6)
 
-        panels[#panels] = api.drawPanel(1, 20, 40, 20, colors.blue, colors.lightBlue, 3)
-        local floggerApp3 = panels[5]:addProgram()
-            :setSize(33, 16)
-            :setPosition(3,3)
-            :execute("programs/fLogger")
+        if tw == 164 then
+            -- Large layout
+            -- Row 1
+            panels[#panels] = api.drawPanel(1, 1, 40, 20, colors.blue, colors.lightBlue, 4)
+
+            local floggerApp = panels[1]:addProgram()
+                :setSize(33, 16)
+                :setPosition(3,3)
+                :execute("programs/fLogger")
+
+            panels[#panels] = api.drawPanel(41, 1, 40, 20, colors.blue, colors.lightBlue, 2)
+            panels[#panels] = api.drawPanel(81, 1, 40, 20, colors.blue, colors.lightBlue, 5)
+            panels[#panels] = api.drawPanel(123, 1, 40, 20, colors.blue, colors.lightBlue, 6)
+
+            
+            -- Row 2
+            panels[#panels] = api.drawPanel(1, 20, 40, 20, colors.blue, colors.lightBlue, 3)
+            local floggerApp3 = panels[5]:addProgram()
+                :setSize(33, 16)
+                :setPosition(3,3)
+                :execute("programs/fLogger")
+            panels[#panels] = api.drawPanel(41, 20, 40, 20, colors.blue, colors.lightBlue, 1)
+            local floggerApp2 = panels[6]:addProgram()
+                :setSize(37, 16)
+                :setPosition(3,5)
+                :execute("programs/fLogger")
+
+            panels[#panels] = api.drawPanel(81, 20, 40, 20, colors.blue, colors.lightBlue, 5)
+            panels[#panels] = api.drawPanel(123, 20, 40, 20, colors.blue, colors.lightBlue, 6)
+
+        elseif tw == 121 then
+            -- Medium Layout
+            panels[#panels] = api.drawPanel(1, 1, 121, 20, colors.blue, colors.lightBlue, 4)
+            local floggerApp = panels[1]:addProgram()
+                :setSize(33, 16)
+                :setPosition(3,3)
+                :execute("programs/fLogger")
 
 
-        panels[#panels] = api.drawPanel(41, 20, 40, 20, colors.blue, colors.lightBlue, 1)
-        panels[#panels] = api.drawPanel(81, 20, 40, 20, colors.blue, colors.lightBlue, 5)
-        panels[#panels] = api.drawPanel(121, 20, 40, 20, colors.blue, colors.lightBlue, 6)
+        elseif tw == 51 then
+            -- Computer Teminal Layout
+            panels[#panels] = api.drawPanel(1, 1, 51, 20, colors.blue, colors.lightBlue, 4)
+            local floggerApp = panels[1]:addProgram()
+                :setSize(33, 16)
+                :setPosition(3,3)
+                :execute("programs/fLogger")
 
+            panels[#panels] = api.drawPanel(41, 1, 40, 20, colors.blue, colors.lightBlue, 2)
+
+        else
+            -- Pocket Computer Layout
+            panels[#panels] = api.drawPanel(1, 1, 26, 20, colors.blue, colors.lightBlue, 4)
+        end
 
     deb = panels[0]:addFrame()
         :setSize(12,1)
-        :setPosition(1,1)
+        :setPosition(2,2)
         :setBackground(colors.black)
+        :setVisible(true)
     deb:addLabel()
-        :setForeground(colors.white)
-        :setText("w: " .. tw .. " h: " .. th)
-
-
+        :setForeground(colors.orange)
+        :setText("W: " .. tw .. " H: " .. th)
     end
 
     return api, basalt
